38ce99cd51c6346143dc25612bbf788615d24d7f
Galina <galinadukaliuk@gmail.com>
2021-10-02 22:32:57 +0300

    add instance w staticIP

---
+++ main.tf
@@ -0,0 +1,21 @@
+resource "google_compute_instance" "instance1" {
+  name         = "instance1"
+  machine_type = "f1-micro"
+  zone         = "europe-west1-b"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+  network_interface {
+    network = "default"
+
+    access_config {
+      nat_ip = google_compute_address.static.address
+    }
+  }
+}
+resource "google_compute_address" "static" {
+  name = "ipv4-address"
+}
